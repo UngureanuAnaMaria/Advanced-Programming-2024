23e71f142e788edddd53972f04a040db3773db12
Ungureanu Ana-Maria <126103134+anaungureanu03@users.noreply.github.com>
2024-03-12 21:38:42 +0200

    Create Church.java

---
+++ Laborator 3/Compulsory/Church.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.time.LocalDate;
+public class Church extends Location implements Visitable {
+    private Map<LocalDate, TimeInterval> timetable;
+
+    public Church(String name, Map<LocalDate, TimeInterval> timetable) {
+        super(name);
+        this.timetable = timetable;
+    }
+
+    public Church() {
+        super("NY");
+        this.timetable = new HashMap<>();
+    }
+
+    @Override
+    public Map<LocalDate,TimeInterval> getTimetable() {
+        return timetable;
+    }
+    public void setTimetable(Map<LocalDate, TimeInterval> timetable) {
+        this.timetable = timetable;
+    }
+
+    @Override
+    public String toString() {
+        return "Church{" +
+                "timetable=" + timetable +
+                '}';
+    }
+}
