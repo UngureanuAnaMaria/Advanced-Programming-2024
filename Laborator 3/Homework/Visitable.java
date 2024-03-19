b11bc19c9d182b6091661b9a66c4951360f9eed9
Ungureanu Ana-Maria <126103134+anaungureanu03@users.noreply.github.com>
2024-03-19 19:27:08 +0200

    Create Visitable.java

---
+++ Laborator 3/Homework/Visitable.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+
+public interface Visitable {
+    Map<LocalDate, TimeInterval> getTimetable();
+    default LocalTime getOpeningHour(LocalDate date) {
+        Map<LocalDate, TimeInterval> timetable = getTimetable();
+        if (timetable != null) {
+            TimeInterval timeInterval = timetable.get(date);
+            if (timeInterval != null) {
+                return timeInterval.getStart();
+            }
+        }
+        return null;
+    }
+
+}
