aa265428409fe4adf28269b731293a4462644aae
Ungureanu Ana-Maria <126103134+anaungureanu03@users.noreply.github.com>
2024-03-19 19:13:51 +0200

    Create Person.java

---
+++ Laborator 4/Compulsory/Person.java
@@ -0,0 +1,51 @@
+package com.yourcompany.model;
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+    private String destination;
+
+    public Person(String name, int age, String destination) {
+        this.name = name;
+        this.age = age;
+        this.destination = destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", destination='" + destination + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        return Integer.compare(this.age, p.getAge());
+    }
+
+}
