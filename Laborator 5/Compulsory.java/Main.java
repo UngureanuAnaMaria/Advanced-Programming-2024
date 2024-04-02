2da4bb5c179cf9eae17ee200833325c6930c0b6c
Ungureanu Ana-Maria <126103134+anaungureanu03@users.noreply.github.com>
2024-04-02 21:47:49 +0300

    Update Main.java

--- Laborator 5/Compulsory.java/Main.java
+++ Laborator 5/Compulsory.java/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.testRepo();
+        //app.testLoadView();
+    }
 
+    private void testRepo() {
+        var repo = new Repository("C:\\Users\\anaun\\OneDrive\\Desktop\\Lab5");
+        // var doc = repo.findDocument("...");
+        repo.loadDocuments();
+        repo.printMap();
+    }
+
+}
