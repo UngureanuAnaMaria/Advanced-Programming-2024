5f3636fdf97bcedc53fc8d23a739c66fa621df96
Ungureanu Ana-Maria <126103134+anaungureanu03@users.noreply.github.com>
2024-04-10 22:52:48 +0300

    Create Main.java

---
+++ Laborator 6/Bonus/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.awt.BorderLayout.CENTER;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+        frame.saveAsImage("C:\\Users\\anaun\\OneDrive\\Desktop\\imgssdcsdcsdnxjk.png");
+    }
+
+}
