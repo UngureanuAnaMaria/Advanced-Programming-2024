e2d37553f90e09b79d92c4de5297c5ffbb5c55b1
Ungureanu Ana-Maria <126103134+anaungureanu03@users.noreply.github.com>
2024-05-13 12:15:10 +0300

    Create Author.java

---
+++ Laborator 9/Compulsory/com.nictas.eclipselink.postgresql/domain/Author.java
@@ -0,0 +1,57 @@
+package com.nictas.eclipselink.postgresql.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "authors")
+@NamedQueries({
+        @NamedQuery(name = "Author.findAll",
+                query = "select e from Author e order by e.name"),
+        @NamedQuery(name = "Author.findByName",
+                query = "SELECT a FROM Author a WHERE a.name = :name"),
+        @NamedQuery(name = "Author.findById",
+                query = "SELECT a FROM Author a WHERE a.id = :id")
+})
+public class Author implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "id", strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name_author")
+    private String name;
+
+    public Author() {
+        // Required by JPA.
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
